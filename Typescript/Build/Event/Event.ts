120b514b7eed1be39f4a4485e6c7bbe68988c304
TakenKills <dragonagario2@gmail.com>
2021-04-11 23:26:42 +0300

    Added once option also emittedFrom option should go soon

--- Typescript/Build/Event/Event.ts
+++ Typescript/Build/Event/Event.ts
@@ -1,15 +1,28 @@
 import { eventOptions } from "../interface/event";
 import { throwErr } from "../Client/client";
+import { events as validEvents } from "../constants";
 
 export class EventBase {
   name: string;
+  emittedFrom?: "commandhandler" | "client";
+  once?: boolean;
+
   constructor(eventOptions: eventOptions) {
-    const { name } = eventOptions;
+    const { name, emittedFrom, once } = eventOptions;
 
     this.name = name;
     if (!this.name || typeof this.name !== "string")
       throwErr(
         `There was no name property provided in the super call of an event or the name provided was not a typeof string.`
       );
+
+    if (!validEvents.includes(this.name))
+      throwErr(`The event "${this.name}" is an invalid event.`);
+
+    this.emittedFrom = emittedFrom;
+    if (!this.emittedFrom) this.emittedFrom = "client";
+
+    this.once = once;
+    if (!this.once) this.once = false;
   }
 }
