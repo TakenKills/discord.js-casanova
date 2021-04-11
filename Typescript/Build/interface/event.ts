4eb83a593daa9c16de3892be5aadbacd7a6de925
TakenKills <dragonagario2@gmail.com>
2021-04-11 23:28:38 +0300

    Event handler set up and ready to go

--- Typescript/Build/interface/event.ts
+++ Typescript/Build/interface/event.ts
@@ -1,10 +1,17 @@
 import { CasanovaClient } from "../Client/client";
 
+type EmittedFrom = "commandhandler" | "client";
+
 export interface eventOptions {
   name: string;
+  emittedFrom?: EmittedFrom;
+  once?: boolean;
 }
 
 export interface Event {
   name: string;
+  emittedFrom?: EmittedFrom;
+  once?: boolean;
   client: CasanovaClient;
+  execute(...args: any): any;
 }
