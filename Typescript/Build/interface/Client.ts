34ebdededdae90f5963f37903ea1aca1c48c1ad3
TakenKills <dragonagario2@gmail.com>
2021-04-11 23:27:28 +0300

    Custom error :o

--- Typescript/Build/interface/Client.ts
+++ Typescript/Build/interface/Client.ts
@@ -1,5 +1,18 @@
 export interface CasanovaOptions {
-    eventHandler: boolean;
-    commandHandler: boolean;
-    token: string;
+  token: string;
+  handlers: Array<"command" | "event">;
+}
+
+export class CasanovaError extends Error {
+  date: Date;
+  constructor(message: string) {
+    super(message);
+
+    Error.captureStackTrace(this, CasanovaError);
+
+    this.name = "CasanovaError";
+    this.message = message;
+    // debugging
+    this.date = new Date();
+  }
 }
