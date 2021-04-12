e1eb613199250950d56e29aca01018e2cb3f64a8
TakenKills <dragonagario2@gmail.com>
2021-04-12 22:24:22 +0300

    Added a reloadEvent function

--- Typescript/Build/interface/eventHandler.ts
+++ Typescript/Build/interface/eventHandler.ts
@@ -1,3 +1,17 @@
+import { Collection } from "discord.js";
+import { CasanovaClient } from "../..";
+import { Event } from "./event";
+
 export interface eventHandlerOptions {
   eventDirectory: string;
+}
+
+export interface EventHandler {
+  client: CasanovaClient;
+  eventDirectory: string;
+  events: Collection<string, Event>;
+  //! add used categories.
+  loadEvent(path: string): void;
+  handleEvent(eventName: string): void;
+  reloadEvent(name: string): Promise<void>;
 }
