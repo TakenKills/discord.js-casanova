d488df9c51ac07b6bc58064562af76c4e3dc3eac
TakenKills <dragonagario2@gmail.com>
2021-04-12 22:23:44 +0300

    Added a path to the event

--- Typescript/Build/interface/event.ts
+++ Typescript/Build/interface/event.ts
@@ -1,17 +1,14 @@
 import { CasanovaClient } from "../Client/client";
 
-type EmittedFrom = "commandhandler" | "client";
-
 export interface eventOptions {
   name: string;
-  emittedFrom?: EmittedFrom;
   once?: boolean;
 }
 
 export interface Event {
   name: string;
-  emittedFrom?: EmittedFrom;
   once?: boolean;
   client: CasanovaClient;
+  path: string;
   execute(...args: any): any;
 }
